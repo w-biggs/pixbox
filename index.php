d1fbf14441a2fec029b0704abc7ff7dd3e29a03e
W Biggs <wilson@wilsonbiggs.com>
2019-03-12 16:10:11 -0400

    Initial plugin header

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Pixbox
+ * Plugin URI: https://github.com/w-biggs/pixbox
+ * Description: A private photo albums plugin.
+ * Author: W Biggs
+ * License: GPL3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: pixbox
+ */
