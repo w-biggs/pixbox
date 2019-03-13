931ecc081a230983bee4763b79a48b24b01e2277
W Biggs <wilson@wilsonbiggs.com>
2019-03-13 00:34:13 -0400

    Improving error handling in action handlers

--- handlers/album_delete.php
+++ handlers/album_delete.php
@@ -6,22 +6,26 @@
  * @since 0.1.0
  */
 add_action('admin_post_pxbx_album_delete', function(){
+  $redir = add_query_arg(array( 
+    'page' => 'pixbox%2Falbums.php',
+    'action' => 'delete'
+  ), 'admin.php');
   if(isset($_POST['album_id'])){
     $album_id = $_POST['album_id'];
+    $redir = add_query_arg(array( 
+      'termid' => $album_id
+    ), $redir);
     $deletion = wp_delete_term($album_id, 'pixbox_albums');
     if(is_wp_error($deletion)){
-      trigger_error($term->get_error_message());
-    } else {
-      $redir = add_query_arg(array( 
-        'page' => 'pixbox%2Falbums.php',
-        'termid' => $term['term_id'],
-        'metaid' => $metaid,
-        'action' => 'delete'
-      ), 'admin.php');
-      wp_redirect($redir);
-      exit;
+      $redir = add_query_arg(array(
+        'error' => $term->get_error_message(),
+      ), $redir);
     }
   } else {
-    var_dump($_POST);
+    $redir = add_query_arg(array(
+      'error' => 'No album ID was given.',
+    ), $redir);
   }
+  wp_redirect($redir);
+  exit;
 });
--- handlers/album_new.php
+++ handlers/album_new.php
@@ -6,11 +6,14 @@
  * @since 0.1.0
  */
 add_action('admin_post_pxbx_album_new', function() use ($pxbx_hasher){
+  $redir = add_query_arg(array( 
+    'page' => 'pixbox%2Falbums.php',
+    'action' => 'add'
+  ), 'admin.php');
   if(isset($_POST['title'])){
     $title = $_POST['title'];
     $parent = 0;
     $pass = null;
-    $metaid = null;
     if(isset($_POST['parent'])){
       $parent = $_POST['parent'];
     }
@@ -18,21 +21,24 @@ add_action('admin_post_pxbx_album_new', function() use ($pxbx_hasher){
       'parent' => $parent
     ));
     if(is_wp_error($term)){
-      trigger_error($term->get_error_message());
+      $redir = add_query_arg(array(
+        'error' => $term->get_error_message(),
+      ), $redir);
     } else {
+      $redir = add_query_arg(array(
+        'termid' => $term['term_id'],
+      ), $redir);
       if(isset($_POST['passcheck']) && isset($_POST['album_pass'])){
         $pass = $_POST['album_pass'];
         $hashedpass = $pxbx_hasher->HashPassword(trim($pass));
-        $metaid = update_term_meta($term['term_id'], 'album_pass', $hashedpass);
+        update_term_meta($term['term_id'], 'album_pass', $hashedpass);
       }
-      $redir = add_query_arg(array( 
-        'page' => 'pixbox%2Falbums.php',
-        'termid' => $term['term_id'],
-        'metaid' => $metaid,
-        'action' => 'add'
-      ), 'admin.php');
-      wp_redirect($redir);
-      exit;
     }
+  } else {
+    $redir = add_query_arg(array(
+      'error' => 'No title was given.',
+    ), $redir);
   }
+  wp_redirect(esc_url($redir));
+  exit;
 });
